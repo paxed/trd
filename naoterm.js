3c83c28001049b9ceb15f16d885f7097b688890e
Pasi Kallinen <paxed@alt.org>
2023-03-11 09:08:11 +0200

    Handle Enhanced symset
    That is, convert utf-8 stream to unicode codepoints

--- naoterm.js
+++ naoterm.js
@@ -728,6 +728,153 @@ function naoterm(wid, hei)
         debugwrite(str);
       }
 
+    // Taken from https://mths.be/punycode
+    this.ucs2decode = function(string) {
+	var output = [];
+	var counter = 0;
+	var length = string.length;
+	var value;
+	var extra;
+	while (counter < length) {
+	    value = string.charCodeAt(counter++);
+	    if (value >= 0xD800 && value <= 0xDBFF && counter < length) {
+		// high surrogate, and there is a next character
+		extra = string.charCodeAt(counter++);
+		if ((extra & 0xFC00) == 0xDC00) { // low surrogate
+		    output.push(((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000);
+		} else {
+		    // unmatched surrogate; only append this code unit, in case the next
+		    // code unit is the high surrogate of a surrogate pair
+		    output.push(value);
+		    counter--;
+		}
+	    } else {
+		output.push(value);
+	    }
+	}
+	return output;
+    }
+
+// Taken from https://mths.be/punycode
+    this.ucs2encode = function(array) {
+	var length = array.length;
+	var index = -1;
+	var value;
+	var output = '';
+	while (++index < length) {
+	    value = array[index];
+	    if (value > 0xFFFF) {
+		value -= 0x10000;
+		output += String.fromCharCode(value >>> 10 & 0x3FF | 0xD800);
+		value = 0xDC00 | value & 0x3FF;
+	    }
+	    output += String.fromCharCode(value);
+	}
+	return output;
+    }
+
+    this.readContinuationByte = function() {
+	if (this.byteIndex >= this.byteCount) {
+	    throw Error('Invalid byte index');
+	}
+
+	var continuationByte = this.byteArray[this.byteIndex] & 0xFF;
+	this.byteIndex++;
+
+	if ((continuationByte & 0xC0) == 0x80) {
+	    return continuationByte & 0x3F;
+	}
+
+	// If we end up here, it’s not a continuation byte
+	throw Error('Invalid continuation byte');
+    }
+
+    this.checkScalarValue = function(codePoint) {
+	if (codePoint >= 0xD800 && codePoint <= 0xDFFF) {
+	    throw Error(
+		'Lone surrogate U+' + codePoint.toString(16).toUpperCase() +
+		    ' is not a scalar value'
+	    );
+	}
+    }
+
+    this.decodeSymbol = function() {
+	var byte1;
+	var byte2;
+	var byte3;
+	var byte4;
+	var codePoint;
+
+	if (this.byteIndex > this.byteCount) {
+	    throw Error('Invalid byte index');
+	}
+
+	if (this.byteIndex == this.byteCount) {
+	    return false;
+	}
+
+	// Read first byte
+	byte1 = this.byteArray[this.byteIndex] & 0xFF;
+	this.byteIndex++;
+
+	// 1-byte sequence (no continuation bytes)
+	if ((byte1 & 0x80) == 0) {
+	    return byte1;
+	}
+
+	// 2-byte sequence
+	if ((byte1 & 0xE0) == 0xC0) {
+	    byte2 = this.readContinuationByte();
+	    codePoint = ((byte1 & 0x1F) << 6) | byte2;
+	    if (codePoint >= 0x80) {
+		return codePoint;
+	    } else {
+		throw Error('Invalid continuation byte');
+	    }
+	}
+
+	// 3-byte sequence (may include unpaired surrogates)
+	if ((byte1 & 0xF0) == 0xE0) {
+	    byte2 = this.readContinuationByte();
+	    byte3 = this.readContinuationByte();
+	    codePoint = ((byte1 & 0x0F) << 12) | (byte2 << 6) | byte3;
+	    if (codePoint >= 0x0800) {
+		this.checkScalarValue(codePoint);
+		return codePoint;
+	    } else {
+		throw Error('Invalid continuation byte');
+	    }
+	}
+
+	// 4-byte sequence
+	if ((byte1 & 0xF8) == 0xF0) {
+	    byte2 = this.readContinuationByte();
+	    byte3 = this.readContinuationByte();
+	    byte4 = this.readContinuationByte();
+	    codePoint = ((byte1 & 0x07) << 0x12) | (byte2 << 0x0C) |
+		(byte3 << 0x06) | byte4;
+	    if (codePoint >= 0x010000 && codePoint <= 0x10FFFF) {
+		return codePoint;
+	    }
+	}
+
+	throw Error('Invalid UTF-8 detected');
+    }
+
+    this.utf8decode = function(byteString)
+    {
+        this.byteArray = this.ucs2decode(byteString);
+	this.byteCount = this.byteArray.length;
+	this.byteIndex = 0;
+	var codePoints = [];
+	var tmp;
+	while ((tmp = this.decodeSymbol()) !== false) {
+	    codePoints.push(tmp);
+	}
+	return this.ucs2encode(codePoints);
+    }
+
+
   this.doescapecode = function(code, param)
       {
 	  debugwrite("doescapecode("+code.toDebugString()+","+param.toDebugString()+")");
@@ -886,7 +1033,12 @@ function naoterm(wid, hei)
 	  var wrotestr = '';
 	  while (idx < str.length) {
 	      if (str.charAt(idx) == '\033') {
-		  if (wrotestr.length > 0) { debugwrite("wrote '<tt style='background-color:#eee;'>"+wrotestr.toDebugString()+"</tt>'"); wrotestr = ''; }
+		  if (wrotestr.length > 0) {
+                      wrotestr = this.utf8decode(wrotestr);
+                      this.putstr(wrotestr);
+                      debugwrite("wrote '<tt style='background-color:#eee;'>"+wrotestr.toDebugString()+"</tt>'");
+                      wrotestr = '';
+                  }
 		  var param = '';
 		  idx++;
 		  switch (str.charAt(idx)) {
@@ -945,13 +1097,18 @@ function naoterm(wid, hei)
 	      } else {
 		  var chr = str.charAt(idx++);
                   var wch = chr;
-                  if (wch == "<") wch = "&lt;";
-                  else if (wch == "&") wch = "&amp;";
+                  //if (wch == "<") wch = "&lt;";
+                  //else if (wch == "&") wch = "&amp;";
 		  wrotestr += wch;
-		  this.putchar(chr);
+		  //this.putchar(chr);
 	      }
 	  }
-	  if (wrotestr.length > 0) { debugwrite("wrote '<tt style='background-color:#eee;'>"+wrotestr.toDebugString()+"</tt>'"); wrotestr = ''; }
+	  if (wrotestr.length > 0) {
+              wrotestr = this.utf8decode(wrotestr);
+              this.putstr(wrotestr);
+              debugwrite("wrote '<tt style='background-color:#eee;'>"+wrotestr.toDebugString()+"</tt>'");
+              wrotestr = '';
+          }
       }
 
 
